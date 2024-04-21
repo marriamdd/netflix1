f7a06a0e4c1ea3ab5074db4fd141d141ca8ba70a
marriamdd <davitashvili.m3@gmail.com>
2024-04-21 20:02:36 +0400

    feat:add axios to get only bookmarkeds on mylist page

--- src/pages/MyListPage.tsx
+++ src/pages/MyListPage.tsx
@@ -1,8 +1,53 @@
+import axios from "axios";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+
 export default function MyListPage() {
-  return <MylistContainer>MyListPage</MylistContainer>;
+  const [marked, setMarked] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    axiosdata();
+  }, []);
+
+  async function axiosdata() {
+    try {
+      let i = 0;
+      while (true) {
+        const response = await axios.get(`http://localhost:3000/${i}`);
+        const data = response.data;
+
+        if (!response || !response.data) {
+          // Break out of the loop if there's no more data
+          break;
+        }
+
+        if (data.isBookmarked) {
+          // Add the marked item to the state
+          setMarked((prevMarked) => [...prevMarked, data]);
+        }
+
+        i++;
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  console.log(marked);
+
+  return (
+    <MylistContainer>
+      {marked.map((item) => (
+        <div key={Math.random()}>
+          <span style={{ color: "red" }}>{item.title}</span>
+        </div>
+      ))}
+    </MylistContainer>
+  );
 }
+
 const MylistContainer = styled.div`
+  padding-top: 8rem;
   color: red;
   background-color: green;
   height: 100vh;
---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+interface Thumbnail {
+  trending: {
+    small: string;
+    large: string;
+  };
+  regular: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+}
+
+interface Movie {
+  title: string;
+  thumbnail: Thumbnail;
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
+}
